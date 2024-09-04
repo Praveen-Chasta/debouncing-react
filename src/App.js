aa17ae8d898c935701e32a58488bbbd9f93cefa6
Praveen-Chasta <praveen.chastaa@gmail.com>
2024-09-04 21:03:19 +0530

    debouncing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect}  from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function App (){
+
+  const [handler, setHandler] = useState('');
+  const [debouncing, setValue] = useState(handler)
+
+  useEffect(() => {
+     const value = setTimeout(() => {
+         setValue(handler)    
+     }, 500)
+
+    return () => (
+      clearTimeout(value)
+    )
+
+  },[handler])
+
+  return(
+    <>
+      <input 
+          type = "text"
+          onChange = {(e) => setHandler(e.target.value)}
+          placeholder = "debouncing..."
+      />
+      <h1>Debouncing Value : {debouncing}</h1>
+    </>
+  )
 }
 
-export default App;
+export default App
